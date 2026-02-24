a123f7a087a99dbb0bba240d29d4bba98c156f67
evilpunkin <82129788+aeughsendhelp@users.noreply.github.com>
2026-02-24 13:02:02 -0800

    model viewer is close to done

--- references/scripts/modelViewer.js
+++ references/scripts/modelViewer.js
@@ -21,17 +21,20 @@ window.IndexModels = function IndexModels() {
 }
 
 
-
 window.openModelViewer = function openModelViewer(modelToOpen) {
     var total = document.getElementById("model-viewer");
     var viewer = document.getElementById("model-viewer-viewer");
-    // var caption = document.getElementById("model-viewer-caption");
+    var description = document.getElementById("model-viewer-description");
+    var download = document.getElementById("model-viewer-download");
 
     total.style.display = "block";
     document.body.classList.add('no-scroll');
 
     viewer.setAttribute('src', modelToOpen.getAttribute("data-model"), 2000)
-    
+    download.href = modelToOpen.getAttribute("data-model");
+
+    description.innerText =  modelToOpen.getAttribute("data-description");
+
     modelViewerIsOpen = true;
     currentmodelIndex = Number(modelToOpen.getAttribute("data-modelIndex"));
 }
---
+++ references/scripts/models.js
@@ -0,0 +1,69 @@
+var models = []
+fetch('/references/models.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Couldn\'t find models.json at ROOT/references/models.json');
+        }
+        return response.json();
+    })
+    .then(data => {
+        for(var modelData of data) {
+            models.push(modelData);
+            console.log("pushed");
+        }
+
+        shuffle(models);
+
+        setModels();
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+
+function setModels() {
+    var models1 = "";
+
+    console.log(models);
+
+    for(var i in models) {
+        var name = models[i].name;
+        var url = models[i].url;
+        var description = models[i].description;
+        var imageurl = models[i].imageurl;
+
+        var modelElement = `
+                <a class="tile viewable-model" data-model="${url}" data-description="${description}">
+                    <h2>${name}</h2>
+                    <img src="${imageurl}">
+                    <p>
+                        ${description}
+                    </p>
+                </a>
+                `;
+
+        models1 += modelElement;
+
+        console.log(modelElement);
+    }
+
+    document.getElementById('models-grid').innerHTML = models1;
+
+    IndexModels();
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
